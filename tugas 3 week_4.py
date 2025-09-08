3149a326324fc2e9904267666d1f5b45b4ec8db6
Devina182 <devinanovitasari@mail.ugm.ac.id>
2025-09-08 14:12:00 +0700

    Add files via upload

---
+++ tugas 3 week_4.py
@@ -0,0 +1,17 @@
+hari = 10
+if (hari == 1):
+    print("Hari Senin")
+elif (hari == 2):
+    print("Hari Selasa")
+elif (hari == 3):
+    print("Hari Rabu")
+elif (hari == 4):
+    print("Hari Kamis")
+elif (hari == 5):
+    print("Hari Jumat")
+elif (hari == 6):
+    print("Hari Sabtu")
+elif (hari == 7):
+    print("Hari Minggu")
+else:
+    print("Hari tidak valid")
