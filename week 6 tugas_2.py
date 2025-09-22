36ed2309f944047f8ea1f379dc1b0fb1fbdbb00f
Devina182 <devinanovitasari@mail.ugm.ac.id>
2025-09-22 14:05:54 +0700

    Add files via upload

---
+++ week 6 tugas_2.py
@@ -0,0 +1,9 @@
+
+nilai = set({3, 6, 9, 2, 5, 7})
+print("Nilai awal:", nilai)
+
+nilai.update([1, 4, 8, 10])
+print("setelah penambahan nilai:", nilai)
+
+nilai ={6,9,2}
+print("setelah penghapusan nilai:", nilai)
