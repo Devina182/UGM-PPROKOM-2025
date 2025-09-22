2cadfa56964e4c1aeab0948b1a9066c18409c106
Devina182 <devinanovitasari@mail.ugm.ac.id>
2025-09-22 14:18:02 +0700

    Add files via upload

---
+++ week 6 tugas_4.py
@@ -0,0 +1,19 @@
+#remove
+angka = {10, 20, 30, 40, 50}
+angka.remove(30)
+print(angka)
+
+#discard 30
+angka = {10, 20, 30, 40, 50}
+angka.discard(30)
+print(angka)
+
+#remove
+angka = {10, 20, 30, 40, 50}
+angka.remove(99)
+print(angka)
+
+#discard 30
+angka = {10, 20, 30, 40, 50}
+angka.discard(30)
+print(angka)
