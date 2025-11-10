58cfd2dadcabb1c19dee8ac6b9248372ed5cbf52
Devina182 <devinanovitasari@mail.ugm.ac.id>
2025-11-10 14:08:52 +0700

    Add files via upload

---
+++ aritmatika.py
@@ -0,0 +1,16 @@
+def penjumlahan(a, b):
+    return a + b
+def pengurangan(a, b):
+    return a - b
+def perkalian(a, b):
+    return a * b
+def pembagian(a, b):
+    if b == 0:
+        return "Error pembagian dengan nol"
+    return a / b
+def modulo(a, b):
+    if b == 0:
+        return "Error tidak bisa modulo dengan nol"
+    return a % b
+def pangkat(a, b):
+    return a ** b
